5319038d7d5f10f5873d9c367c5b0e6bd24c2ed7
KolyaVSJ <kolya.van.sint.jan@ulb.be>
2024-05-23 14:45:23 +0200

    mémoir message

--- response.py
+++ response.py
@@ -1,5 +1,7 @@
 from openai import OpenAI
 
+messages = []
+
 def get_response(user_input:str, ai:OpenAI) -> str:
     lowered: str = user_input.lower()
 
@@ -7,11 +9,11 @@ def get_response(user_input:str, ai:OpenAI) -> str:
         return "Silence..."
     
     else:
+        messages.append({"role":"user", "content":user_input})
         completition = ai.chat.completions.create(
             model="gpt-3.5-turbo-0125",
-            messages=[
-                {"role":"user", "content":user_input} 
-            ]
+            messages=messages
         )
-        print(completition.choices[0].message)
+        print(completition.choices[0].message.role)
+        messages.append({"role" : completition.choices[0].message.role, "content":completition.choices[0].message.content})
         return completition.choices[0].message.content
